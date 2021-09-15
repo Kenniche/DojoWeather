43fe806b61f8805ac0b0335f0a9333c152fa16e1
Kenniche <kenniche23@gmail.com>
2021-09-15 09:08:18 -0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+function cookieRemove(elem){
+    elem.parentNode.remove(elem);
+}
+
+function changingCity(){
+    alert('Loading weather report...')
+}
+
+
+
+//let test = document.querySelectorAll(".degrees")
+
+//function changeUnit(element){
+    
+  //  for (var i =0; i < test.length; i++){
+     //   let degreesWeather = test[i].innerText;
+      //  if (element.value === "farenheit"){
+      //      degreesWeather =  degreesWeather*(9/5) + 32;
+      //      test[i].innerText =  Math.round(degreesWeather);
+     //   } 
+      //  else if (element.value === "celsius"){
+      //      degreesWeather =  (degreesWeather - 32) * (5/9);
+      //     test[i].innerText =  Math.round(degreesWeather);
+      //  }
+  //  }
+//}
